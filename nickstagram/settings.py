88fb7005d649bc586af00527970fbd4fc750b77c
NickKirilov <nickkirilov04@gmail.com>
2022-04-09 12:04:03 +0300

    Added set-up for Allowed hosts in setting.py.

--- nickstagram/settings.py
+++ nickstagram/settings.py
@@ -16,7 +16,11 @@ SECRET_KEY = 'django-insecure-=04d^$$)tc!xli-v5_7nbas=szbgva4m+15q(kshws$*qns15z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost:8000',
+    'https://nickstagram.herokuapp.com',
+]
 
 # Application definition
 
