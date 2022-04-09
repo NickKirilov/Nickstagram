8be4ba0151bf22e4f76b45c9c230724911d59cc8
NickKirilov <nickkirilov04@gmail.com>
2022-04-09 11:52:07 +0300

    Updated settings.py file

--- nickstagram/settings.py
+++ nickstagram/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
